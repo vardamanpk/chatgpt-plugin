50cb952eeee55531f7bb2a9426875ddfca81c4ce
vardamanpk <vardamanpk@gmail.com>
2023-07-24 23:56:27 +0530

    Update ai-plugin.json
    Update inv-spec.json
    
    Update index.js
    
    Update index.js
    
    Update ai-plugin.json
    
    Update ai-plugin.json
    
    Update ai-plugin.json
    
    Update index.js
    
    Update ai-plugin.json
    
    Update ai-plugin.json
    
    Forward oauth request
    
    Print error
    
    Import axios
    
    Update package.json
    
    Update package-lock.json
    
    Update ai-plugin.json
    
    Update ai-plugin.json
    
    Create inv-spec.yaml
    
    Update ai-plugin.json
    
    Update inv-spec.yaml
    
    Update ai-plugin.json
    
    Update inv-spec.json
    
    Update ai-plugin.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const axios = require('axios');
 const cors = require("cors");
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
@@ -6,10 +7,32 @@ const app = express();
 const port = 3000;
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.json());
 
 app.use("/exclude-test", (req, res) => {
   res.send("served from local server");
 });
+
+app.post("/v1/oauth", async (req, res) => {
+  console.log('body: ', req.body)
+  console.log('header: ', JSON.stringify(req.headers))
+  const auth = Buffer.from(`${req.body.client_id}:${req.body.client_secret}`).toString('base64');
+    const headers = {
+        'Authorization': `Basic ${auth}`,
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    const data = `grant_type=authorization_code&code=${req.body.code}&redirect_uri=${encodeURIComponent(req.body.redirect_uri)}`;
+
+    try {
+        const response = await axios.post('https://api-m.sandbox.paypal.com/v1/oauth2/token', data, { headers });
+        console.log('res:', response.data);
+        return res.json(response.data);
+    } catch (error) {
+        console.log('error:', error);
+        return res.status(500).json({ error: error.toString() });
+    }
+})
 
 app.use(
   "/",
